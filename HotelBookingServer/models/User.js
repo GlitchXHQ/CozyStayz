93a8d5a3d3fd9795584cb27f9069e6b7e5fadd25
GlitchXHQ <er.vivekchauhan1@gmail.com>
2025-11-18 10:58:11 +0530

    fix: user schema validation error

--- HotelBookingServer/models/User.js
+++ HotelBookingServer/models/User.js
@@ -1,14 +1,17 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const userSchema=mongoose.Schema({
-    _id:{type:String,required:true},
-    username:{type:String,required:true},
-    email:{type:String,required:true},
-    image:{type:String,required:true},
-    role:{type:String,enum:["user","hotelOwner"],default:"user"},
-    recentSearchedCities:[{type:String,required:true}]
-},{timestamps:true})
+const userSchema = mongoose.Schema({
+  _id: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  image: { type: String, required: true },
+  role: { type: String, enum: ["user", "hotelOwner"], default: "user" },
+  recentSearchedCities: {
+    type: [String],
+    default: []
+  }
+}, { timestamps: true })
 
-const User= mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema)
 
-module.exports=User
+module.exports = User
