4b6d37335f9d5b28b928b9dc3e6dc780f06e55bb
GlitchXHQ <er.vivekchauhan1@gmail.com>
2025-11-18 10:52:07 +0530

    temp rename webhook

--- HotelBookingServer/api/clerk/webhook_tmp.js
+++ HotelBookingServer/api/clerk/webhook_tmp.js
@@ -4,10 +4,10 @@ export const config = {
   },
 };
 
-const { Webhook } = require("svix");
-require("dotenv").config();
-const database = require("../../config/database");
-const User = require("../../models/User");
+const { Webhook } = require("svix")
+require("dotenv").config()
+const database = require("../../config/database")
+const User = require("../../models/User")
 
 database();
 
